099ad75c19cb06171572331b99e30b245cc5e722
peterband2002 <105830877+peterband2002@users.noreply.github.com>
2022-05-19 21:23:33 -0300

    aperfeiçoando

--- index.php
+++ index.php
@@ -3,4 +3,12 @@ echo "Olá mundo dos fortes";
 
 echo "Sou demais e também lindo!";
 
+$nome1 = "Pedim da Babilônia";
+$sobrenome = "Bandeirantes";
+$nomeCompl = $nome1 . " " . $sobrenome;
+
+echo $nomeCompl . "<br>";
+
+exit;
+
 ?>
